ec1a53e4517b9fba06952d1d716949da6f166d2b
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-01-02 18:39:53 -0500

    Add tests

---
+++ test/test_instinct.js
@@ -0,0 +1,80 @@
+instinct=require("../instinct")
+assert = require("assert")
+vows=require("vows")
+
+var logic = {
+  A:function(cb) { setTimeout(function() { cb(4) },200)},
+  B: function(A,cb) { cb(A+5)},
+  C: 10
+}
+
+var I = instinct(logic);
+
+vows.describe("instinct").addBatch({
+  'instinct by function' : {
+    topic : function() {
+      var cb = this.callback;
+      I.time = new Date();
+      I.exec(function(B) {
+        cb(null,B)
+      })
+    },
+    "returns correct variable" : function(d) {
+      assert.equal(d,9)
+    },
+    "waits for execution" : function(d) {
+      var wait = (new Date()) -I.time
+      assert.equal(wait > 200,true,"wait was "+wait)
+    },
+    "Memoize results" : {
+      topic : function() {
+        var cb = this.callback
+        I.time = new Date()
+        I.exec(function(B) {
+          cb(null,B)
+        })
+      },
+      "The time to fetch again is zero" : function(d) {
+        var wait = (new Date()) -I.time
+        assert.equal(wait<5,true,"wait was "+wait)
+      }
+    }
+  },
+  'instinct by name' : {
+    topic : function() {
+      var cb = this.callback;
+      I.exec("B",function(d) { cb(null,d)})
+    },
+    'returns correct variable' : function(d) {
+      assert.equal(d,9)
+    }
+  },
+  "default value in logic" : {
+    topic : function() {
+      var cb = this.callback;
+      I.exec("C",function(d) {
+        cb(null,d)
+      })
+    },
+    "default value is returned" : function(d) {
+      assert.equal(d,10)
+    },
+    "fact table is updated" : function(d) {
+      assert.equal(I.facts['C'],10)
+    }
+  },
+  "undefined parameter" : {
+    topic : function() {
+      var cb = this.callback;
+      I.exec("E",function(d) {
+        cb(null,d)
+      })
+    },
+    "returns undefined" : function(d) {
+      assert.strictEqual(d,undefined)
+    }
+
+  }
+})
+
+.export(module)
